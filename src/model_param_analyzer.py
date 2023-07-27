2531e515aa2957d1ff12cabf33fb9073e78a54a0
guangdadong.dong <ghostyuan@outlook.com>
2023-07-27 16:42:33 +0800

    alalysis entry

--- src/model_param_analyzer.py
+++ src/model_param_analyzer.py
@@ -13,7 +13,10 @@ class Model_param_analyzer():
     self.operator_freq_dict = {}
     self.featuremap_freq_dict = {}
     self.kernel_freq_dict = {}
+    self.filter_freq_dict = {}
     self.featuremap_size_freq_dict= {}
+    self.param_freq_dict = {}
+    self.mulAdd_freq_dict = {}
 
   def layers_key_frequency(self, model_layers_table:dict, key_index:int=0):
     result_dict = {}
@@ -24,7 +27,7 @@ class Model_param_analyzer():
 
     return result_dict
 
-  def show_freq_statictis(self, operator_freq_dict, kernel_freq_dict, featuremap_freq_dict, featuremap_size_freq_dict):
+  def show_freq_statictis(self, operator_freq_dict, kernel_freq_dict, filter_freq_dict, featuremap_freq_dict, featuremap_size_freq_dict, param_freq_dict, mulAdd_freq_dict):
     divider = "=" * 50
     summary_str = f'\n     operator_freq_dict: {len(operator_freq_dict)}\n\n'
     sorded_operator_freq = sorted(operator_freq_dict.items(), key=lambda d: d[1], reverse=False)
@@ -47,6 +50,25 @@ class Model_param_analyzer():
     summary_str += f'\n     featuremap_size_freq_dict: {len(featuremap_size_freq_dict)}\n\n'
     sorted_featuremap_freq = sorted(featuremap_size_freq_dict.items(), key=lambda d: d[0], reverse=False)
     for key,value in sorted_featuremap_freq:
+      summary_str += f"{key:{30}}: {value:{10}}\n"
+    summary_str += divider+ '\n'
+
+    summary_str += f'\n     filter_freq_dict: {len(filter_freq_dict)}\n\n'
+    sorded_kernel_freq = sorted(filter_freq_dict.items(), key=lambda d: d[1], reverse=False)
+    for key,value in sorded_kernel_freq:
+      summary_str += f"{key:{30}}: {value:{10}}\n"
+    summary_str += divider+ '\n'
+
+    summary_str += f'\n     param_freq_dict: {len(param_freq_dict)}\n\n'
+    sorded_kernel_freq = sorted(param_freq_dict.items(), key=lambda d: d[1], reverse=False)
+    for key,value in sorded_kernel_freq:
+      summary_str += f"{key:{30}}: {value:{10}}\n"
+    summary_str += divider+ '\n'
+
+
+    summary_str += f'\n     mulAdd_freq_dict: {len(mulAdd_freq_dict)}\n\n'
+    sorded_kernel_freq = sorted(mulAdd_freq_dict.items(), key=lambda d: d[1], reverse=False)
+    for key,value in sorded_kernel_freq:
       summary_str += f"{key:{30}}: {value:{10}}\n"
     summary_str += divider+ '\n'
 
@@ -77,8 +99,12 @@ class Model_param_analyzer():
     layers_operator_freq_dict = self.layers_key_frequency(model_layers_table, 0)
     layers_kernel_freq_dict = self.layers_key_frequency(model_layers_table, 1)
     layers_featuremap_freq_dict = self.layers_key_frequency(model_layers_table, 2)
+    layers_filter_freq_dict = self.layers_key_frequency(model_layers_table, 4)
     layers_featuremap_size_freq_dict = self.layers_featuremap_size_frequency(model_layers_table)
-    self.show_freq_statictis(layers_operator_freq_dict, layers_kernel_freq_dict, layers_featuremap_freq_dict, layers_featuremap_size_freq_dict)
+    layers_param_freq_dict = self.layers_key_frequency(model_layers_table, 5)
+    layers_mulAdd_freq_dict = self.layers_key_frequency(model_layers_table, 7)
+    self.show_freq_statictis(layers_operator_freq_dict, layers_kernel_freq_dict, layers_filter_freq_dict,
+                             layers_featuremap_freq_dict, layers_featuremap_size_freq_dict, layers_param_freq_dict, layers_mulAdd_freq_dict)
 
     write_table(model_layers_table, xlsx_file_name, model_name)
     self.layers_table_list.append(model_layers_table)
@@ -116,9 +142,12 @@ class Model_param_analyzer():
     self.key_frequency(frequency_dict=self.kernel_freq_dict, key_index=1)
     self.key_frequency(frequency_dict=self.featuremap_freq_dict, key_index=2)
     self.featuremap_size_frequency(frequency_dict=self.featuremap_size_freq_dict)
+    self.key_frequency(frequency_dict=self.filter_freq_dict, key_index=4)
+    self.key_frequency(frequency_dict=self.param_freq_dict, key_index=5)
+    self.key_frequency(frequency_dict=self.mulAdd_freq_dict, key_index=7)
 
   def show_analysis_statictis(self):
-    self.show_freq_statictis(self.operator_freq_dict, self.kernel_freq_dict, self.featuremap_freq_dict, self.featuremap_size_freq_dict)
+    self.show_freq_statictis(self.operator_freq_dict, self.kernel_freq_dict, self.filter_freq_dict, self.featuremap_freq_dict, self.featuremap_size_freq_dict, self.param_freq_dict, self.mulAdd_freq_dict)
 
   def draw_from_dict(self, data_dict, title='title', output_dir='.', sort_item=1):
     by_value = sorted(data_dict.items(),key = lambda item:item[sort_item],reverse=True)
@@ -145,7 +174,10 @@ class Model_param_analyzer():
     self.draw_from_dict(self.operator_freq_dict,    prefix+'_operators_frequence',  'output')
     self.draw_from_dict(self.featuremap_freq_dict,  prefix+'_featuremap_frequency', 'output')
     self.draw_from_dict(self.kernel_freq_dict,      prefix+'_kernel_frequency',     'output')
+    self.draw_from_dict(self.filter_freq_dict,      prefix+'_filter_frequency',     'output')
     self.draw_from_dict(self.featuremap_size_freq_dict, prefix+'_featuremap_size_frequency', 'output', 0)
+    self.draw_from_dict(self.param_freq_dict,       prefix+'_param_frequency',      'output')
+    self.draw_from_dict(self.mulAdd_freq_dict,      prefix+'_mulAdd_frequency',     'output')
 
 def main():
   data_dict = {'ReLU':289, 'Conv2d':341, 'BatchNorm2d':1565, 'Linear':655, 'AdaptiveAvgPool2d':1337, 'Padding':226, 'MaxPool2d':399, 'Hardswish':967, 'Softmax':405}
